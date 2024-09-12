4d09568454fff92c9eb7b838e2519eebca73660f
ice-wzl <75596877+ice-wzl@users.noreply.github.com>
2024-09-11 22:09:49 -0400

    initial commit

---
+++ release_tree/dev_release.py
@@ -0,0 +1,5 @@
+dev_release = [
+    "7.1rc7", "7.1rc6", "7.1rc5", "7.1rc4", "7.1rc3", "7.1rc2", "7.1rc1",
+    "7.1beta6", "7.1beta5", "7.1beta4", "7.1beta3", "7.1beta2", "7.1beta1",
+    "7.0.9", "7.0beta8", "7.0beta7", "7.0beta5", "7.0beta4", "7.0beta3"
+]
